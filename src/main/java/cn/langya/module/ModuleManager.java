b2d567b311d1b33242df7e6099e786a948afa6d4
LangYa466 <langya466@gmail.com>
2024-12-28 08:54:09 +0800

    feat(module): 添加模块管理器功能并完善注释说明

--- src/main/java/cn/langya/module/ModuleManager.java
+++ src/main/java/cn/langya/module/ModuleManager.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Manages all modules and their interactions.
+ * 管理所有模块及其交互.
  *
  * @author LangYa
  * @since 2024/11/16 03:46
@@ -20,22 +20,45 @@ import java.util.Map;
 public class ModuleManager {
     private final Map<String, Module> moduleMap = new HashMap<>();
 
+    /**
+     * 构造函数，初始化模块管理器.
+     */
     public ModuleManager() {
         init();
     }
 
+    /**
+     * 添加模块到模块地图.
+     *
+     * @param module 要添加的模块
+     */
     public void addModule(Module module) {
         moduleMap.put(module.getName(), module);
     }
 
+    /**
+     * 根据模块名获取模块.
+     *
+     * @param moduleName 模块名称
+     * @return 返回对应的模块
+     */
     public Module getModule(String moduleName) {
         return moduleMap.get(moduleName);
     }
 
+    /**
+     * 根据模块类获取模块.
+     *
+     * @param moduleClass 模块类
+     * @return 返回对应的模块
+     */
     public Module getModule(Class<?> moduleClass) {
         return moduleMap.get(moduleClass.getSimpleName());
     }
 
+    /**
+     * 初始化模块管理器，注册模块并设置事件监听.
+     */
     private void init() {
         InitializerUtil.initialize(clazz -> {
             if (InitializerUtil.check(Module.class, clazz)) {
@@ -50,6 +73,11 @@ public class ModuleManager {
         Client.getInstance().getEventManager().register(this);
     }
 
+    /**
+     * 处理键盘输入事件，切换相应模块状态.
+     *
+     * @param event 键盘输入事件
+     */
     @EventTarget
     public void onKeyInput(EventKeyInput event) {
         moduleMap.values().stream()
@@ -57,3 +85,4 @@ public class ModuleManager {
                 .forEach(Module::toggle);
     }
 }
+
